28c29a24973998f7add6a6ae8e34be42b217dd90
NamgyungKim <namgyung.kim@gmail.com>
2021-10-25 23:54:13 +0900

    Initialise Project
    React+Firebase Setup

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDn9WZnDUpPclTYp1YyOVwqzSKayQ4g6bU",
+  authDomain: "nwiter-c4a3f.firebaseapp.com",
+  projectId: "nwiter-c4a3f",
+  storageBucket: "nwiter-c4a3f.appspot.com",
+  messagingSenderId: "756646998371",
+  appId: "1:756646998371:web:bd76737ed6504e156c6356"
+};
+
+export default firebase.initializeApp(firebaseConfig);
+
