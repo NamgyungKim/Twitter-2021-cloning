6a5a87bfebf62a94989b40842cf98738f1d98ae8
NamgyungKim <namgyung.kim@gmail.com>
2021-10-27 00:02:46 +0900

    Router Setup

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,4 @@
-import * as firebase from "firebase/app";
+import * as firebase from 'firebase/app'
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -6,8 +6,7 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
-  appId: process.env.REACT_APP_APP_ID
-};
+  appId: process.env.REACT_APP_APP_ID,
+}
 
-export default firebase.initializeApp(firebaseConfig);
-
+export default firebase.initializeApp(firebaseConfig)
